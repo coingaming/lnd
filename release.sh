5e013f88190d909ac69b896940628d349c079ab4
Ilja Tkachuk <ilja@heathmont.net>
2020-05-18 14:10:36 +0300

    CI

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 set -e
 
 for V in "v0.10.0-beta"; do
-  if [ "$1" == "--push" ]; then
+  if [ "$1" = "--push" ]; then
     docker push "heathmont/lnd:$V"
   else
     docker build . --build-arg checkout="$V" -t "heathmont/lnd:$V"
