d690513517f8e45291883af9c9889da6192beca2
Ilja Tkachuk <ilja@heathmont.net>
2020-05-18 14:24:43 +0300

    docker tag per commit

--- release.sh
+++ release.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+COMMIT="$(git rev-parse --verify HEAD --short)"
+
 for V in "v0.10.0-beta"; do
+  TAG="heathmont/lnd:$V-$COMMIT"
   if [ "$1" = "--push" ]; then
-    docker push "heathmont/lnd:$V"
+    docker push "$TAG"
   else
-    docker build . --build-arg checkout="$V" -t "heathmont/lnd:$V"
+    docker build . --build-arg checkout="$V" -t "$TAG"
   fi
 done
